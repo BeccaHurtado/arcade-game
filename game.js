9351f866907a1fff9fb5b7e4eb29a92b05fe8e08
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-14 18:19:38 -0700

    added movement to red ball

--- game.js
+++ game.js
@@ -1,11 +1,32 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
 
 window.onload = function() {
     console.log("Hello World!")
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
 
+    var framesPerSecond = 30;
+    setInterval(function() {
+        moveEverything();
+        loadEverything();
+    }, 1000/framesPerSecond);    
+}
+
+function moveEverything() {
+    ballX = ballX + 5;
+}
+
+function loadEverything() {
+
+    canvasContext.fillStyle = 'black';
+    // 0px from the left, 0px from the right. Size of object in the canvas.
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(0, 210, 10, 100);
+
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(ballX,100, 10, 10);
 }
